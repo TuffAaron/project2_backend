efda4410480eaa3a52afe7499ffbc3ff92ff9ab5
TuffAaron <aarperez@csumb.edu>
2025-10-10 11:04:40 -0700

    Simplified GameController to match expected return in the frontend

--- project2_backend/src/main/groovy/com/example/demo/controller/GameController.java
+++ project2_backend/src/main/groovy/com/example/demo/controller/GameController.java
@@ -1,18 +1,14 @@
 package com.example.demo.controller;
 
-import java.time.LocalDate;
 import com.example.demo.model.Game;
 import java.util.List;
 import com.example.demo.service.GameService;
-import java.time.ZonedDateTime;
-import java.time.ZoneId;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -25,13 +21,7 @@ public class GameController {
     private GameService gameService;
 
     @GetMapping("/team/{teamId}")
-    public List<Game> getGamesByTeamAndDate(
-        @PathVariable Long teamId,
-        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
-    ){
-        ZonedDateTime startDateTime = startDate.atStartOfDay(ZoneId.of("America/New_York"));
-        ZonedDateTime endDateTime = endDate.atTime(23, 59, 59).atZone(ZoneId.of("America/New_York"));
-        return gameService.getGamesByTeamAndDate(teamId, startDateTime, endDateTime);
+    public List<Game> getGamesByTeam(@PathVariable Long teamId){
+        return gameService.getGamesByTeam(teamId);
     }
 }
