f9add3a4c5c7490f0ebb51c5e1c8c8c4d5c827bf
Micah <micahheneveld@gmail.com>
2025-10-11 17:17:33 -0700

    Polished API for database implementation

--- project2_backend/src/main/groovy/com/example/demo/Project2BackendApplication.groovy
+++ project2_backend/src/main/groovy/com/example/demo/Project2BackendApplication.groovy
@@ -2,18 +2,10 @@ package com.example.demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
 
-@Controller
 @SpringBootApplication
 class Project2BackendApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
 	static void main(String[] args) {
 		SpringApplication.run(Project2BackendApplication, args)
 	}
